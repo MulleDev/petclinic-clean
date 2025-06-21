74be6462b823078a0a7f38737ca57b593df4e2f0
Dennis <mullekular@gmail.com>
2025-06-21 21:45:08 +0200

    chore: apply spring-javaformat for Heroku compliance

--- src/main/java/org/springframework/samples/petclinic/owner/PetTypeController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetTypeController.java
@@ -7,16 +7,18 @@ import java.util.List;
 
 @Controller
 public class PetTypeController {
-    private final PetTypeRepository petTypeRepository;
 
-    public PetTypeController(PetTypeRepository petTypeRepository) {
-        this.petTypeRepository = petTypeRepository;
-    }
+	private final PetTypeRepository petTypeRepository;
 
-    @GetMapping("/pet-types")
-    public String listPetTypes(Model model) {
-        List<PetType> petTypes = petTypeRepository.findPetTypes();
-        model.addAttribute("petTypes", petTypes);
-        return "petTypes";
-    }
+	public PetTypeController(PetTypeRepository petTypeRepository) {
+		this.petTypeRepository = petTypeRepository;
+	}
+
+	@GetMapping("/pet-types")
+	public String listPetTypes(Model model) {
+		List<PetType> petTypes = petTypeRepository.findPetTypes();
+		model.addAttribute("petTypes", petTypes);
+		return "petTypes";
+	}
+
 }
